0d51c5fc5f41d1e2fe4b8260ac0cdb5ab2f1bcbc
yongkie.purnomo <yongkie.purnomo@doku.com>
2025-02-15 19:32:19 +0700

    initial commit

---
+++ src/main/java/com/alphabet/simple/payment/PaymentApiListenerApplication.java
@@ -0,0 +1,13 @@
+package com.alphabet.simple.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentApiListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentApiListenerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alphabet/simple/payment/PaymentApiListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alphabet.simple.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApiListenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
